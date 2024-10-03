fc61763d5c007e0a8732d1f32c9069dc0a790a04
2948351 <shining@kia.com>
2024-10-03 22:37:54 +0900

    Trino 연결 테스트 (lib 경로 수정)

--- trino_baseline.py
+++ trino_baseline.py
@@ -9,7 +9,7 @@ from airflow.utils.weight_rule import WeightRule
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.operators.dummy import DummyOperator
-from lib.util import TrinoOperator
+from .lib.util import TrinoOperator
 from airflow.contrib.hooks.ssh_hook import SSHHook
 
 
