22749074726dc310b636ff0a516de743ee33240e
2948351 <shining@kia.com>
2024-10-03 22:10:55 +0900

    Trino 연결 테스트

---
+++ lib/util.py
@@ -0,0 +1,25 @@
+from airflow.models.baseoperator import BaseOperator
+from trino.dbapi import connect
+
+
+class TrinoOperator(BaseOperator):
+    def __init__(self, query: str, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.host = "opyter.iptime.org"
+        self.port = 20001
+        self.user = "airflow"
+        self.catalog = "dl_iceberg"
+        self.schema = "stg"
+        self.query = query
+
+    def execute(self, context):
+        conn = connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            catalog=self.catalog,
+            schema=self.schema,
+        )
+
+        cur = conn.cursor(self.query)
+        cur.execute(self.query)
